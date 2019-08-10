a301c19aa325949b5f6513e943eed34baf43b323
Glutanimate <glutanimate@users.noreply.github.com>
2019-08-10 19:41:00 +0200

    Move add-on properties into global ADDON namespace
    Following suite with latest libaddon changes

--- src/memobeam/consts.py
+++ src/memobeam/consts.py
@@ -38,26 +38,44 @@ from __future__ import (absolute_import, division,
 
 from ._version import __version__
 
+try:
+    from .data.patrons import MEMBERS_CREDITED, MEMBERS_TOP
+except ImportError:
+    MEMBERS_CREDITED = MEMBERS_TOP = ()
+
 __all__ = [
-    "ADDON_NAME", "ADDON_ID", "ADDON_VERSION", "LINKS",
-    "LICENSE", "LIBRARIES", "AUTHORS", "CONTRIBUTORS",
-    "SPONSORS"
+    "ADDON"
 ]
 
-# ADD-ON
+# PROPERTIES DESCRIBING ADDON
 
-ADDON_NAME = "MemoBeam"
-ADDON_ID = ""
-ADDON_VERSION = __version__
-LINKS = {
-    "help": ""
-}
-LICENSE = "GNU AGPLv3"
-LIBRARIES = []
-AUTHORS = [
-    {"name": "Glutanimate (Glutanimate", "years": "2019",
-     "contact": "https://glutanimate.com"}
-]
-# automatically sorted:
-CONTRIBUTORS = []
-SPONSORS = []
+
+class ADDON(object):
+    """Class storing general add-on properties
+    Property names need to be all-uppercase with no leading underscores
+    """
+    NAME = "MemoBeam"
+    MODULE = "memobeam"
+    ID = "1809412029"
+    VERSION = __version__
+    LICENSE = "GNU AGPLv3"
+    AUTHORS = (
+        {"name": "Glutanimate", "years": "2019",
+         "contact": "https://glutanimate.com"},
+    )
+    AUTHOR_MAIL = "ankiglutanimate@gmail.com"
+    LIBRARIES = ()
+    CONTRIBUTORS = ()
+    SPONSORS = ()
+    MEMBERS_CREDITED = MEMBERS_CREDITED
+    MEMBERS_TOP = MEMBERS_TOP
+    LINKS = {
+        "patreon": "https://www.patreon.com/glutanimate",
+        "bepatron": "https://www.patreon.com/bePatron?u=7522179",
+        "coffee": "http://ko-fi.com/glutanimate",
+        "description": "https://ankiweb.net/shared/info/{}".format(ID),
+        "rate": "https://ankiweb.net/shared/review/{}".format(ID),
+        "twitter": "https://twitter.com/glutanimate",
+        "youtube": "https://www.youtube.com/c/glutanimate",
+        "help": ""
+    }
